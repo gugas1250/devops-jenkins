73529a661888ef205e174c9a93ad8ff1f7559d52
Paulo Peres <88409939+gugas1250@users.noreply.github.com>
2022-03-09 21:19:28 -0800

    third

---
+++ third_job.sh
@@ -0,0 +1,5 @@
+echo "test1" > ~/jenkins_chain_job/file1.txt
+echo "test2" > ~/jenkins_chain_job/file2.txt
+echo "test3" > ~/jenkins_chain_job/file3.txt
+echo "test4" > ~/jenkins_chain_job/file4.txt
+echo "test5" > ~/jenkins_chain_job/file5.txt
